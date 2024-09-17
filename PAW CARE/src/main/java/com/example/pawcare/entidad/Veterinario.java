1660c66649c6f91faab3ccffc0782d5ca84d5ad8
Andrés Florez <86497740+florezandres@users.noreply.github.com>
2024-09-17 16:26:37 -0500

    Delete PAW CARE directory

--- PAW CARE/src/main/java/com/example/pawcare/entidad/Veterinario.java
+++
@@ -1,5 +0,0 @@
-package com.example.pawcare.entidad;
-
-public class Veterinario {
-    
-}
--- PAW CARE/src/main/java/com/example/pawcare/repositorio/AdministradorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.pawcare.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.pawcare.entidad.Administrador;
-
-public interface AdministradorRepository extends JpaRepository<Administrador, Long> {
-    Administrador findByCedula(int cedula);
-}
